a96c6ac00036dc78e9f32f4a4ddef412449394a5
erozonachi <erozonachi.james@gmail.com>
2019-07-26 13:58:24 +0100

    Create and export jokes-container styled component

---
+++ react-client/src/components/styled-components/JokesContainer.js
@@ -0,0 +1,33 @@
+import styled from 'styled-components';
+
+export const JokesContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  max-width: 500px;
+  width: 100%;
+  margin: 0 auto;
+  > h2 {
+    margin: 10px 0;
+  }
+  > ul {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    width: 100%;
+    padding: 10px 0;
+    list-style-type: none;
+
+    > li {
+      outline: none;
+      padding: 10px;
+      border-bottom: 1px solid blueviolet;
+      color: blueviolet;
+      width: 95%;
+      margin: 5px 2.5%;
+      list-style-type: none;
+    }
+  }
+`;
