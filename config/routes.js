70f7590c0fa4aa313e42cf9466c99ad7a4162ff8
erozonachi <erozonachi.james@gmail.com>
2019-07-26 09:24:35 +0100

    Implement the login function
    Implement the `login` function inside `/config/routes.js`.

--- config/routes.js
+++ config/routes.js
@@ -1,6 +1,8 @@
 const axios = require('axios');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 const usersModel = require('../database/models/users');
+const secret = require('./secret').jwtSecret;
 
 const { authenticate } = require('../auth/authenticate');
 
@@ -36,7 +38,7 @@ async function register(req, res) {
       json(createdUser);
       
   } catch(error) {
-    if(err.errno === 19) {
+    if(error.errno === 19) {
       return res.status(400)
         .json({ message: 'Supplied username field already exists' });
     }
@@ -47,8 +49,36 @@ async function register(req, res) {
   }
 }
 
-function login(req, res) {
+async function login(req, res) {
   // implement user login
+  try {
+    const { username, password } = req.body;
+
+    const user = usersModel.getByUsername(username);
+    if(bcrypt.compareSync(password, user.password)) {
+      const payload = {
+        sub: user.disposer,
+        username: user.username
+      }
+      const token = jwt.sign(payload, secret, { expiresIn: '1d' });
+
+      return res.status(200)
+        .json({
+          message: 'Login successful',
+          token
+        });
+    }
+    res.status(400)
+      .json({
+        message: 'Invalid login credentials'
+      });
+      
+  } catch(error) {
+    res.status(500)
+      .json({
+        error: 'Cannot complete user login, try again'
+      });
+  }
 }
 
 function getJokes(req, res) {
