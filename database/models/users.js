bd1e4f6b7c8c7eb5f78d4e18bb0d77d0d7609f17
erozonachi <erozonachi.james@gmail.com>
2019-07-26 08:54:01 +0100

    Add insert function to the users model module

--- database/models/users.js
+++ database/models/users.js
@@ -1,5 +1,9 @@
 const db = require('../dbConfig');
 
 module.exports = {
-  
+  insert: function(user) {
+    return db('users')
+      .insert(user)
+      .then(([ id ]) => this.getById(id));
+  },
 }
